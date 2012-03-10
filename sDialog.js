9f398537d3c347adef44c6d82b454733f2f21d12
Tatsh <ddrtist@gmail.com>
2012-03-10 11:32:27 -0600

    More functionality

--- sDialog.js
+++ sDialog.js
@@ -1,3 +1,7 @@
+/**
+ * @constructor
+ * @param {string} title
+ */
 var sDialog = function (title) {
   this.parent.constructor.call(this);
 
@@ -16,13 +20,24 @@ var sDialog = function (title) {
 
   this._buttons = [];
   this._buttonsContainer = document.createElement('div');
+  this._buttonsContainer.className = 'sdialog-button-container';
+
+  this._centered = true;
 
   return this;
 };
 sDialog.makeCloseButton = function () {
-  var close = document.createElement('span');
+  var close = document.createElement('a');
   close.className = 'close-button-x';
+  close.setAttribute('href', '#');
   close.innerHTML = 'X';
+
+  q(close).bind('click', function (event) {
+    sHistory.removeState('dialog');
+    event.preventDefault();
+    return false;
+  });
+
   return close;
 };
 sDialog.prototype = new sView();
@@ -59,17 +74,18 @@ sDialog.prototype.setId = function (id) {
   return this;
 };
 sDialog.prototype.hide = function () {
-  this._DOMElement.className += ' sdialog-hidden';
+  this._DOMElement.className += ' sdialog-hidden';;
   return this;
 };
 sDialog.prototype.show = function () {
-  this._DOMElement.className = this._DOMElement.className.replace(/(\s+)?sdialog\-hidden/, '');
+  this._DOMElement.className = this._DOMElement.className.replace(/(\s+)?sdialog\-hidden/g, '');
   return this;
 };
 sDialog.prototype.addButton = function (label, formToSubmit) {
   var button = document.createElement('input');
   button.setAttribute('type', 'submit');
   button.setAttribute('value', label);
+  button.className = 'form-submit';
 
   q(button).bind('click', function (event) {
     formToSubmit.getDOMElement().submit();
@@ -80,6 +96,8 @@ sDialog.prototype.addButton = function (label, formToSubmit) {
   return this;
 };
 sDialog.prototype.appendTo = function (element) {
+  var addEvent = !this.isAppended() && this._centered;
+
   this.parent.appendTo.call(this, element);
 
   for (var i = 0; i < this._buttons.length; i++) {
@@ -88,5 +106,32 @@ sDialog.prototype.appendTo = function (element) {
 
   this._DOMElement.appendChild(this._buttonsContainer);
 
+  if (addEvent) {
+    var element = this._DOMElement;
+    var resize = function () {
+      var height = sWin.getHeight() / 2;
+      var width = sWin.getWidth() / 2;
+      var x = parseInt(width - (element.offsetWidth / 2), 10);
+      var y = parseInt(height - (element.offsetHeight / 2), 10);
+
+      if (sCSS.hasTransforms) {
+        sCSS.translate(element, x, y);
+      }
+      else {
+        element.style.left = x + 'px';
+        element.style.top = y + 'px';
+      }
+    };
+    resize();
+    sWin.bind('resize', resize, false);
+  }
+
+  return this;
+};
+sDialog.prototype.setWidth = function (width, unit) {
+  if (unit === undefined) {
+    unit = 'px';
+  }
+  this._DOMElement.style.width = width + unit;
   return this;
 };
