bdbd203c6069cfde49cfb741a4a015439fb6f2b8
Tatsh <ddrtist@gmail.com>
2012-03-18 18:19:40 -0500

    Fix method name; adjust default style/properties

--- sOverlayView.js
+++ sOverlayView.js
@@ -14,16 +14,40 @@ var sOverlayView = function () {
   this._DOMElement.className = 'soverlay';
 
   /**
+   * @type sOverlayView
+   * @private
+   */
+  var instance = this;
+
+  /**
+   * Default opacity.
+   * @private
+   * @type number
+   */
+  this._opacity = 0.6;
+
+  /**
    * @type Object
    * @private
    */
   var css = {
     'display': 'none',
-    'zIndex': -2000
+    'zIndex': -2000,
+    'width': sWin.getWidth() + 'px',
+    'position': 'fixed',
+    'top': 0,
+    'height': sWin.getHeight() + 'px',
+    'opacity': 0
   };
   for (var attr in css) {
     this._DOMElement.style[attr] = css[attr];
   }
+
+  sWin.addEventListener('resize', function () {
+    var el = instance.getDOMElement();
+    el.style.height = sWin.getHeight() + 'px';
+    el.style.width = sWin.getWidth() + 'px';
+  });
 
   return this;
 };
@@ -39,13 +63,17 @@ sOverlayView.prototype.parent = sView.prototype;
  */
 sOverlayView.prototype.show = function () {
   this._DOMElement.style.display = 'block';
+  this._DOMElement.style.zIndex = 100;
+  this._DOMElement.style.opacity = this._opacity;
   return this;
 };
 /**
  * Hide the overlay.
  * @returns {sOverlayView} The object to allow method chaining.
  */
-sOverlayView.prototype.show = function () {
+sOverlayView.prototype.hide = function () {
+  this._DOMElement.style.opacity = 0;
+  this._DOMElement.style.zIndex = -2000;
   this._DOMElement.style.display = 'none';
   return this;
 };
