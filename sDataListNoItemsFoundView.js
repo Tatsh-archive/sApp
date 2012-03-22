b6a1d0fa49d983c348e6d7d9d7e9fb0bdef56115
Tatsh <ddrtist@gmail.com>
2012-03-22 12:25:12 -0500

    Add a view for when no items found in a data list

---
+++ sDataListNoItemsFoundView.js
@@ -0,0 +1,14 @@
+/**
+ * A view for when no items are found in a data source.
+ * @constructor
+ * @returns {sDataListNoItemsFoundView} The view.
+ */
+var sDataListNoItemsFoundView = function () {
+  this.parent.constructor.call(this);
+  this._DOMElement = sDoc.newElement('li');
+  this._DOMElement.className = 'sdata-list-no-items-found';
+  this._DOMElement.innerHTML = 'No items found.';
+  return this;
+};
+sDataListNoItemsFoundView.prototype = new sDataListItem();
+sDataListNoItemsFoundView.prototype.parent = sDataListItem.prototype;
