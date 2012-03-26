7b0e7d693d544f0b6ec79664c811ed0e4943c631
Tatsh <ddrtist@gmail.com>
2012-03-26 14:28:13 -0500

    Add sButton; make sButtonBar use that; update order file

---
+++ sButton.js
@@ -0,0 +1,168 @@
+/**
+ * Represents a button.
+ * @param {string} [text] Text to display.
+ * @param {function(sButton)} [fn] The callback.
+ * @returns {sButton} The button object.
+ */
+var sButton = function (text, fn) {
+  this.parent.constructor.call(this);
+
+  /**
+   * @type string
+   * @private
+   */
+  this._text = text ? text : '';
+  /**
+   * @type boolean
+   * @private
+   */
+  this._enabled = true;
+  /**
+   * @type Element
+   * @private
+   */
+  this._DOMElement = sDoc.newElement('a');
+  /**
+   * @type {function(sButton)}
+   * @private
+   */
+  this._userClickHandler = typeof fn === 'function' ? fn : function () {};
+  /**
+   * @type sButton
+   * @private
+   */
+  var button = this;
+  /**
+   * @this {Element}
+   * @private
+   */
+  this._clickHandler = function (event) {
+    button._userClickHandler(button);
+    event.preventDefault();
+    return false;
+  };
+
+  // Render
+  this._DOMElement.setAttribute('href', '#');
+  this._DOMElement.setAttribute('role', 'button');
+  this._DOMElement.appendChild(document.createTextNode(text));
+  q(this._DOMElement).bind('click', this._clickHandler);
+  this._setClasses();
+
+  return this;
+};
+/**
+ * @type sView
+ * @private
+ */
+sButton.prototype = new sView();
+sButton.prototype.parent = sView.prototype;
+/**
+ * Set the text.
+ * @param {string} text The text.
+ * @returns {sButton} The object to allow method chaining.
+ */
+sButton.prototype.setText = function (text) {
+  this._text = text ? text : '';
+
+};
+/**
+ * @returns {sButton} The object to allow method chaining.
+ * @private
+ */
+sButton.prototype._setClasses = function () {
+  if (this._enabled) {
+    this._DOMElement.className = 'sbutton';
+    return this;
+  }
+
+  this._DOMElement.className = 'sbutton sbutton-disabled';
+
+  return this;
+};
+/**
+ * Set the click handler function.
+ * @param {function(sButton)} fn The callback.
+ * @returns {sButton} The object to allow method chaining.
+ */
+sButton.prototype.setClickHandler = function (fn) {
+  this._userClickHandler = fn;
+  return this;
+};
+/**
+ * If the button is disabled.
+ * @returns {boolean} If the button is disabled.
+ */
+sButton.prototype.isDisabled = function () {
+  return !this._enabled;
+};
+/**
+ * If the button is enabled.
+ * @returns {boolean} If the button is enabled.
+ */
+sButton.prototype.isEnabled = function () {
+  return this._enabled;
+};
+/**
+ * Disables the button. Adds a class <code>sbutton-disabled</code> that can
+ *   be used to style the button in this state.
+ * @param {Array} [classes] Other classes to add.
+ * @returns {sButton} The object to allow method chaining.
+ */
+sButton.prototype.disable = function (classes) {
+  var oldClasses = this._DOMElement.className.split(' ');
+  var i;
+
+  if (classes && classes.length) {
+    var newClasses = ['sbutton', 'sbutton-disabled'];
+    var i;
+
+    for (i = 0; i < oldClasses.length; i++) {
+      newClasses.push(fUTF8.trim(oldClasses[i]));
+    }
+
+    for (i = 0; i < classes.length; i++) {
+      newClasses.push(classes[i]);
+    }
+
+    this._DOMElement.className = newClasses.join(' ');
+
+    return this;
+  }
+
+  this._DOMElement.setAttribute('aria-disabled', false);
+  this._setClasses();
+
+  return this;
+};
+/**
+ * Disables the button. Adds a class <code>sbutton-disabled</code> that can
+ *   be used to style the button in this state.
+ * @param {Array} [classes] Other classes to add.
+ * @returns {sButton} The object to allow method chaining.
+ */
+sButton.prototype.disable = function (classes) {
+  var oldClasses = this._DOMElement.className.split(' ');
+
+  if (classes && classes.length) {
+    var newClasses = ['sbutton'];
+    var i;
+
+    for (i = 0; i < oldClasses.length; i++) {
+      newClasses.push(fUTF8.trim(oldClasses[i]));
+    }
+
+    for (i = 0; i < classes.length; i++) {
+      newClasses.push(classes[i]);
+    }
+
+    this._DOMElement.className = newClasses.join(' ');
+
+    return this;
+  }
+
+  this._DOMElement.setAttribute('aria-disabled', true);
+  this._setClasses();
+
+  return this;
+};
--- sButtonBar.js
+++ sButtonBar.js
@@ -36,40 +36,33 @@ sButtonBar.prototype.parent = sView.prototype;
 /**
  * Adds a button.
  * @param {string} label Label text.
- * @param {function()} fn Callback.
+ * @param {function(sButton)} fn Callback.
  * @returns {sButtonBar} The object to allow method chaining.
  */
 sButtonBar.prototype.addButton = function (label, fn) {
-  var eventHandler = function (event) {
-    fn();
-    event.preventDefault();
-    return false;
-  };
-
   var li = q(sDoc.newElement('li'));
   var realLi = li.get();
-  var a = q(sDoc.newElement('a')).setAttributes({
-    'href': '#'
-  }).bind('click', eventHandler).setText(label);
-
-  li.append(a);
+  var button = new sButton(label, fn);
 
   this._childElements[label] = realLi;
-  this._eventHandlers[label] = eventHandler;
+  this._eventHandlers[label] = fn;
+
+  button.appendTo(realLi);
   this._DOMElement.appendChild(realLi);
 
   return this;
 };
-/**
- * Removes a button.
- * @param {string} label Label of the button to remove.
- * @returns {sButtonBar} The object to allow method chaining.
- */
-sButtonBar.prototype.removeButton = function (label) {
-  if (this._childElements[label]) {
-    var eventHandler = this._eventHandlers[label];
-    q(this._childElements[label]).unbind('click', eventHandler).remove();
-  }
-
-  return this;
-};
+// TODO Implement.
+// /**
+//  * Removes a button.
+//  * @param {string} label Label of the button to remove.
+//  * @returns {sButtonBar} The object to allow method chaining.
+//  */
+// sButtonBar.prototype.removeButton = function (label) {
+//   if (this._childElements[label]) {
+//     var eventHandler = this._eventHandlers[label];
+//     q(this._childElements[label]).unbind('click', eventHandler).remove();
+//   }
+//
+//   return this;
+// };
